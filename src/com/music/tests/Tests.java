6579372064cf759a942149f466a4760dc40ffb2e
Linette V Maliakal <lmaliakal21@gmail.com>
2020-04-12 23:21:32 -0500

    added empty tests for patterns
    Signed-off-by: Linette V Maliakal <lmaliakal21@gmail.com>

--- src/com/music/tests/Tests.java
+++ src/com/music/tests/Tests.java
@@ -12,6 +12,41 @@ import com.music.model.subscribers.*;
 
 public class Tests {
 	
+	NewManager newManager = new NewManager();
+	OldManager oldManager = new OldManager();
+	Collaborator collabsub = new Collaborator();
+	Fan fansub = new Fan();
 	
+	@BeforeEach
+	void setUp() throws Exception {
+		newManager.getStatus();
+		oldManager.getStatus();
+		collabsub.getStatus();
+		fansub.getStatus();
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	
+	@Test
+	void testOnTourManager() {
+		
+	}
+	
+	@Test
+	void testOffTourManager() {
+		
+	}
+	
+	@Test
+	void testRegister() {
+		
+	}
+	
+	@Test
+	void testUnregister() {
+		
+	}
 
 }
